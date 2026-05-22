514e5103ccdd90d988d586d9b1411f96229074da
hyoyh <135303230+hyo-suzuki@users.noreply.github.com>
2026-05-22 13:55:57 +0900

    Change Bluetooth device filter prefix from 'R02' to 'C'

--- site/src/ringBle.ts
+++ site/src/ringBle.ts
@@ -154,7 +154,7 @@ export class RingBleClient {
       throw new Error("Web Bluetooth is not available in this browser.");
     }
     const device = await navigator.bluetooth.requestDevice({
-      filters: [{ namePrefix: "R02" }],
+      filters: [{ namePrefix: "C" }],
       optionalServices: [UART_SERVICE_UUID, DFU_SERVICE_UUID, DEVICE_INFO_SERVICE_UUID, "device_information"],
     });
     this.device = device;
